2ae7e83ab9212d3ebb1cbecb1899aad1eaad114b
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-21 08:17:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function createTable() {
     //Write your code here
+	var row=prompt("Input number of rows");
+	var col=prompt('Input number of columns');
+	var table=document.getElementById('myTable');
+	for(let i=0;i<row;i++){
+		var r=document.createElement('tr');
+		for(let j=0;j<col;j++){
+			var c=document.createElement('td');
+			c.innerText=`Row-${i} Column-${j}`;
+			r.appendChild(c);
+		}
+		table.appendChild(r);
+	}
   
 }
